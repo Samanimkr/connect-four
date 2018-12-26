470fc12d1c6290d489450baaa97d5916da185f4e
Samanimkr <samanimkr@outlook.com>
2018-12-26 21:56:58 +0000

    chip inserts in the right place and pointer bar

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+// CONFIG
 const GRID_COLUMNS = 7;
 const GRID_ROWS = 6;
-/*
- * 0 = Empty
- * 1 = Player one
- * 2 = Player two
- */
-const GRID_DATA = [];
-const CONTAINER = document.querySelector(".container");
+const GRID_DATA = []; // 0 = Empty, 1 = Player one, 2 = Player two
 
+// ELEMENTS SELECTORS
+const $CONTAINER = document.querySelector(".container");
+const $BOXES_WRAPPER = document.querySelector(".boxes_wrapper");
+const $POINTER = document.querySelector(".pointer");
+
+const POINTER_POSITIONS = ['15px', '105px', '195px', '285px', '375px', '465px', '555px', '645px'];
 var CURRENT_PLAYER = 1;
 
 // INITIALISING
@@ -22,49 +23,46 @@ var CURRENT_PLAYER = 1;
     for (let i = 0; i < (GRID_COLUMNS*GRID_ROWS); i++) {
         boxesHtml += `<button class="box" id="box_${i}"></button>`;
     }
-    CONTAINER.innerHTML = boxesHtml;
+    $BOXES_WRAPPER.innerHTML = boxesHtml;
 })();
-const BOXES = document.querySelectorAll('button');
+const $BOXES = document.querySelectorAll('button');
 
 
 // EVENT LISTENER
-BOXES.forEach(box => {
-    // box.addEventListener("mouseover", function() {
-    //     const boxNum = this.id.substr(4);
-    //     const column = boxNum % 7;
-    //     this.classList.add('red');
-    // }); ------------- PADDING * COLUMN NUMBER ------------------
-    // box.addEventListener("mouseleave", function() {
-    //     this.classList.remove('red');
-    // });
+$BOXES.forEach(box => {
+    box.addEventListener("mouseover", function() {
+        const boxNum = this.id.substr(4);
+        const column = boxNum % 7;
+        $POINTER.style.left = POINTER_POSITIONS[column];
+    });
     box.addEventListener("click", function() {
         const boxNum = this.id.substr(4);
         const currentColumn = boxNum % 7;
-        insertChipInColumn(currentColumn);
-        console.log(GRID_DATA)
+        const response = insertChipProcedure(currentColumn);
+        if (response && response === -1) {
+            console.log('Column is full!')
+        }
     });
 })
 
-function insertChipInColumn(column) {
+function insertChipProcedure(column) {
     const lastBoxInColumn = 5;
-    console.log('lastBoxInColumn: ', lastBoxInColumn);
 
     for (let row = lastBoxInColumn; row >= 0; row--) {
-        console.log('column: ', column);
-        console.log('row: ', row);
         const boxStatus = GRID_DATA[column][row];
         if (boxStatus === 0) {
             GRID_DATA[column][row] = CURRENT_PLAYER;
-            console.log('BOX: ', (column+1)*(row+1));
-            const box = document.getElementById(`box_${(column+1)*(row+1)}`);
-            insertChipColour(box);
-            return true;
+
+            const index = (row * 7) + column;
+            const box = document.getElementById(`box_${index}`);
+            insertChip(box);
+            return;
         }
     }
-    return false;
+    return -1;
 };
 
-function insertChipColour(box) {
+function insertChip(box) {
     if(CURRENT_PLAYER === 1) {
         box.classList.add('yellow');
         CURRENT_PLAYER = 2;
