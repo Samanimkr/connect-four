46b7340d09b295fb527049772a789ffce34e4419
Samanimkr <samanimkr@outlook.com>
2018-12-27 14:29:39 +0000

    Winning procedure - Checking horizontally & vertically

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-// CONFIG
+/*
+ * CONFIG
+ */
 const GRID_COLUMNS = 7;
 const GRID_ROWS = 6;
 const GRID_DATA = []; // 0 = Empty, 1 = Player one, 2 = Player two
 
-// ELEMENTS SELECTORS
+
+/*
+ * ELEMENTS SELECTORS
+ */
+const $PLAYER_TURN = document.querySelector(".player_turn");
 const $CONTAINER = document.querySelector(".container");
 const $BOXES_WRAPPER = document.querySelector(".boxes_wrapper");
 const $POINTER = document.querySelector(".pointer");
@@ -12,10 +18,13 @@ const POINTER_POSITIONS = [15, 105, 195, 285, 375, 465, 555, 645];
 var POINTER_CURRENT_POSITION = 0;
 var CURRENT_PLAYER = 1;
 
-// INITIALISING
+
+/*
+ * INITIALISING
+ */
 (function initialiseGridData() {
     for (let col = 0; col < GRID_COLUMNS; col++) {
-        GRID_DATA[col] = [0, 0, 0, 0, 0, 0, 0];
+        GRID_DATA[col] = [0, 0, 0, 0, 0, 0];
     }
 })();
 
@@ -29,7 +38,28 @@ var CURRENT_PLAYER = 1;
 const $BOXES = document.querySelectorAll('button');
 
 
-// EVENT LISTENER
+/*
+ * ANIMATIONS
+ */
+function movePointer(currentPos, finalPos) {
+    var pos = currentPos;
+    var id = setInterval(frame, 5);
+
+    function frame() {
+        if (pos == finalPos) {
+            animationInProgess = false;
+            clearInterval(id);
+        } else {
+            pos = pos < finalPos? pos+5 : pos-5; 
+            $POINTER.style.left = pos + 'px'; 
+        }
+    }
+}
+
+
+/*
+ * EVENT LISTENERS
+ */
 $BOXES.forEach(box => {
     box.addEventListener("mouseover", function() {
         const boxNum = this.id.substr(4);
@@ -52,6 +82,10 @@ $BOXES.forEach(box => {
     });
 })
 
+
+/*
+ * INSERTING CHIP PROCEDURE
+ */
 function insertChipProcedure(column) {
     const lastBoxInColumn = 5;
 
@@ -62,36 +96,108 @@ function insertChipProcedure(column) {
 
             const index = (row * 7) + column;
             const box = document.getElementById(`box_${index}`);
-            insertChip(box);
+            if(CURRENT_PLAYER === 1) box.classList.add('yellow');
+            if(CURRENT_PLAYER === 2) box.classList.add('red');
+
+            checkForWinner(column, row);
+            changeTurn();
             return;
         }
     }
     return -1;
 };
 
-function insertChip(box) {
+function changeTurn() {
     if(CURRENT_PLAYER === 1) {
-        box.classList.add('yellow');
         CURRENT_PLAYER = 2;
+        $PLAYER_TURN.innerHTML = 'Player 2';
+        $PLAYER_TURN.style.backgroundColor = "#d73c2c";
         $POINTER.style.backgroundColor = "#d73c2c";
     } else {
-        box.classList.add('red');
         CURRENT_PLAYER = 1;
+        $PLAYER_TURN.innerHTML = 'Player 1';
+        $PLAYER_TURN.style.backgroundColor = "#f1c40f";
         $POINTER.style.backgroundColor = "#f1c40f";
     }
 }
 
-function movePointer(currentPos, finalPos) {
-    var pos = currentPos;
-    var id = setInterval(frame, 5);
+/*
+ * WINNING PROCEDURE
+ */
+function checkForWinner(column, row) {
+    let playerHasWon = false;
+    if (!playerHasWon) playerHasWon = checkHorizontal(column, row);
+    if (!playerHasWon) playerHasWon = checkVertical(column, row);
 
-    function frame() {
-      if (pos == finalPos) {
-          animationInProgess = false;
-        clearInterval(id);
-      } else {
-        pos = pos < finalPos? pos+5 : pos-5; 
-        $POINTER.style.left = pos + 'px'; 
-      }
+    // Diagonals
+    // if (row >= 2 && column >= 3 && !playerHasWon) { // Top Left
+    //     playerHasWon = checkTopLeft(column, row);
+    // }
+    // if (row <= 2 && column >= 3 && !playerHasWon) { // Bottom Left
+    //     playerHasWon = checkBottomLeft(column, row);
+    // }
+    // if (row >= 2 && column <= 3 && !playerHasWon) { // Top Right
+    //     playerHasWon = checkTopRight(column, row);
+    // }
+    // if (row <= 2 && column <= 3 && !playerHasWon) { // Bottom Right
+    //     playerHasWon = checkBottomRight(column, row);
+    // }
+    if (playerHasWon) console.log(CURRENT_PLAYER + ' is the WINNER');
+}
+
+// GROUP DIRECTIONS
+function checkVertical(column, row) {
+    let counter = 1;
+    if (row <= 2) { // Bottom
+        counter += checkBottom(column, row);
     }
-  }
+    return (counter >= 4);
+};
+
+function checkHorizontal(column, row) {
+    let counter = 1;
+    if (column >= 3) { // Left
+        counter += checkLeft(column, row);
+    }
+    if (column <= 3 && counter < 4) { // Right
+        counter += checkRight(column, row);
+    }
+    return (counter >= 4);
+};
+
+// INDIVIDUAL DIRECTIONS - MOVE TO A NEW FILE!!!
+function checkBottom(column, row){
+    let counter = 1, flag = true;
+    while (counter < 4 && flag) {
+        if (GRID_DATA[column][row+counter] === CURRENT_PLAYER) {
+            counter++;
+        } else {
+            flag = false;
+        }
+    }
+    return counter-1;
+}
+
+function checkLeft(column, row){
+    let counter = 1, flag = true;
+    while (counter < 4 && flag) {
+        if (GRID_DATA[column-counter][row] === CURRENT_PLAYER) {
+            counter++;
+        } else {
+            flag = false;
+        }
+    }
+    return counter-1;
+}
+
+function checkRight(column, row){
+    let counter = 1, flag = true;
+    while (counter < 4 && flag) {
+        if (GRID_DATA[column+counter][row] === CURRENT_PLAYER) {
+            counter++;
+        } else {
+            flag = false;
+        }
+    }
+    return counter-1;
+}
